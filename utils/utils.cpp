3923363134302c305573a0321c66f31ed85d0324
Maicss-chen <maicss@126.com>
2022-07-10 17:30:02 +0800

    删减无用代码

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -3,17 +3,16 @@
 //
 #include <QMessageBox>
 #include <QFileInfo>
-#include <sys/stat.h>
 #include <QTextStream>
-#include <QDir>
 #include <QFileDialog>
+#include <QCryptographicHash>
 
 #include "utils.h"
 
-#include <stdlib.h>
-#include <QCryptographicHash>
+#include <cstdlib>
+#include <sys/stat.h>
 
-QString getDirPath(QString filename) {
+QString getDirPath(const QString& filename) {
     return filename.left(filename.lastIndexOf('/'));
 }
 
@@ -21,7 +20,7 @@ QString HomeDir() {
     return getenv("HOME");
 }
 
-void MessageBoxExec(QString title, QString content, QMessageBox::Icon icon) {
+void MessageBoxExec(const QString& title, const QString& content, QMessageBox::Icon icon) {
     QMessageBox messageBox;
     messageBox.setWindowTitle(title);
     messageBox.setText(content);
@@ -46,25 +45,7 @@ bool mkdirP(QString path) {
     return true;
 }
 
-void lsDir(QString path, QVector<Entry> *list, QString front, Category category) {
-    QDir dir(path);
-    for(auto entry : dir.entryList()){
-        if (entry == "." || entry == "..") continue;
-        QFileInfo info(path + "/" + entry);
-        if (info.isDir()){
-            list->append(Entry{front+entry,DIR,"",category});
-            lsDir(path+"/"+entry,list,front+entry+"/",category);
-            continue;
-        }
-        if (info.isSymLink()){
-            list->append(Entry{front+entry,SYMLINK,info.symLinkTarget(),category});
-            continue;
-        }
-        list->append(Entry{front+entry,NORMAL_FILE,"",category});
-    }
-}
-
-QString getMd5(QString path) {
+QString getMd5(const QString& path) {
     QFile file(path);
     file.open(QFile::ReadOnly);
     QByteArray ba = QCryptographicHash::hash(file.readAll(), QCryptographicHash::Md5);
@@ -77,7 +58,7 @@ QString chooseDirectory() {
     return QFileDialog::getExistingDirectory();
 }
 
-QString chooseFile(QString filter) {
+QString chooseFile(const QString& filter) {
     return QFileDialog::getOpenFileName(nullptr,"",HomeDir(),filter);
 }
 
--- utils/utils.h
+++ utils/utils.h
@@ -8,31 +8,14 @@
 #include <QString>
 #include <QMessageBox>
 
-enum FileType {
-    NORMAL_FILE,
-    DIR,
-    SYMLINK
-};
-enum Category {
-    GAME,
-    DATA
-};
-struct Entry {
-    QString path;
-    FileType type;
-    QString target;
-    Category category;
-};
-
-QString getDirPath(QString filename);
+QString getDirPath(const QString& filename);
 
 QString HomeDir();
-void MessageBoxExec(QString title, QString content, QMessageBox::Icon icon = QMessageBox::Icon::NoIcon);
+void MessageBoxExec(const QString& title, const QString& content, QMessageBox::Icon icon = QMessageBox::Icon::NoIcon);
 bool mkdirP(QString path);
-void lsDir(QString, QVector<Entry> *, QString front, Category category);
-QString getMd5(QString path);
+QString getMd5(const QString& path);
 QString chooseDirectory();
-QString chooseFile(QString filter);
+QString chooseFile(const QString& filter);
 QString getValidParentPath(QString path);
 
 #endif //GAMEINSTALLER_UTILS_H
